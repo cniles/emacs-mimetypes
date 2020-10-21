a8a15b0e7dc83ee5a00e17e8463a535e05a19b74
Craig Niles <craig.niles@cloudreach.com>
2020-10-21 12:07:24 -0400

    Fix spelling.

--- mimetypes.el
+++ mimetypes.el
@@ -25,7 +25,7 @@
 
 ;;; Commentary:
 
-;; mimetypes provides a library for guessing mimetype by file
+;; mimetypes provides a library for guessing MIME type by file
 ;; extension based on the platform.  On Windows it will search the
 ;; registry.  Otherwise, it will search for one of the known locations
 ;; that a mime.types file may exist and utilize it.
